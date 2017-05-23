aa50fdd45f837c3c0d93f1c01e682548d21678fe
landonrs <landonrs@users.noreply.github.com>
2017-05-22 21:32:34 -0600

    Update Screen classes and added tools package

--- core/src/com/teamcadia/mathcadia/Screens/LibraryScreen.java
+++ core/src/com/teamcadia/mathcadia/Screens/LibraryScreen.java
@@ -1,7 +1,43 @@
 package com.teamcadia.mathcadia.Screens;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by Richardo on 5/22/2017.
  */
-public class LibraryScreen {
+public class LibraryScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
---
+++ core/src/com/teamcadia/mathcadia/Tools/MapObjectCreator.java
@@ -0,0 +1,7 @@
+package com.teamcadia.mathcadia.Tools;
+
+/**
+ * Created by Richardo on 5/22/2017.
+ */
+public class MapObjectCreator {
+}

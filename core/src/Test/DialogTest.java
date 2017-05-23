30c4d65e8ed8af8e92684ae53aa74d392c0904c0
landonrs <landonrs@users.noreply.github.com>
2017-05-23 11:03:10 -0600

    Added Hud class and test package with milestone tests

---
+++ core/src/Test/DialogTest.java
@@ -0,0 +1,7 @@
+package Test;
+
+/**
+ * Created by Richardo on 5/23/2017.
+ */
+public class DialogTest {
+}
---
+++ core/src/com/teamcadia/mathcadia/Tools/Hud.java
@@ -0,0 +1,17 @@
+package com.teamcadia.mathcadia.Tools;
+
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * Created by Richardo on 5/22/2017.
+ */
+public class Hud implements Disposable {
+
+
+
+
+    @Override
+    public void dispose() {
+
+    }
+}
